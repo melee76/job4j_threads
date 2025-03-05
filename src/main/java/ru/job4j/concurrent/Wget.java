176928f5d393a004a5b9fc9af69b275ce06c310c
zoubarevna <melee76@yandex.ru>
2025-03-05 11:32:00 +0300

    4. Режим ожидания. [#231217 #534899]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -4,12 +4,11 @@ public class Wget {
     public static void main(String[] args) {
         Thread thread = new Thread(
                 () -> {
-                    for (int index = 0; index < 100; index++) {
-                    try {
-                            System.out.print("\rLoading : " + index + "%");
-                            Thread.sleep(1000);
-                        }
-                     catch(InterruptedException e){
+                    for (int index = 0; index <= 100; index++) {
+                        try {
+                        System.out.print("\rLoading : " + index + "%");
+                        Thread.sleep(1000);
+                        } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
