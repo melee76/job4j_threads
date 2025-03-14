59fc558bcd35a0ef0d2cf683d350389fa3e533f6
zoubarevna <melee76@yandex.ru>
2025-03-14 09:33:09 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/thread/ParallelSearch.java
@@ -0,0 +1,33 @@
+package ru.job4j.thread;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        Thread slave = new Thread(() -> {
+            for (int index = 0; index != 3; index++) {
+                try {
+                    queue.offer(index);
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        slave.start();
+        slave.join();
+        consumer.interrupt();
+    }
+}
